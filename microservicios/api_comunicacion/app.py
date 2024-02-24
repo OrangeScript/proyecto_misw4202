3a073f5bd674c01a46c00c6ab7ac3f63675a85eb
Juan Rodriguez <jp.rodriguez22@uniandes.edu.co>
2024-02-23 21:02:22 -0500

    Coordinar hilos de Waitress a 5

--- microservicios/api_comunicacion/app.py
+++ microservicios/api_comunicacion/app.py
@@ -21,4 +21,4 @@ def add_urls(app):
 
 if __name__ == '__main__':    
     app = create_flask_app()
-    serve(app=app, host='127.0.0.1', port=5002, threads=8, connection_limit=200)
+    serve(app=app, host='127.0.0.1', port=5002, threads=5, connection_limit=200)
